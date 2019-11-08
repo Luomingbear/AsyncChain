637a72487cc74feb240cc6ed340cace152de4ad7
luoming <luomingbear@163.com>
2019-11-08 11:20:07 +0800

    初始化

---
+++ src/main/java/com/bearever/async/chain/lifecycler/base/AsyncChainLifeCycleListener.java
@@ -0,0 +1,21 @@
+package com.bearever.async.chain.lifecycler.base;
+
+
+import android.app.Activity;
+
+/**
+ * 生命周期状态的基类
+ *
+ * @author :  malong    luomingbear@163.com
+ * @date :  2019/7/24
+ **/
+public interface AsyncChainLifeCycleListener {
+
+    /**
+     * 当 {@link android.app.Fragment#onDestroy()}} 或者 {@link
+     * Activity#onDestroy()}  的时候执行，表示依附的对象销毁了
+     *
+     * @param lifeCycle 被销毁的生命周期类
+     */
+    void onDestroy(AsyncChainLifeCycle lifeCycle);
+}
---
+++ src/main/java/com/bearever/async/chain/lifecycler/iml/ApplicationLifeCycle.java
@@ -0,0 +1,33 @@
+package com.bearever.async.chain.lifecycler.iml;
+
+
+import com.bearever.async.chain.lifecycler.base.AsyncChainLifeCycle;
+import com.bearever.async.chain.lifecycler.base.AsyncChainLifeCycleListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * app运行期间都存在的生命周期实现
+ *
+ * @author :  malong    luomingbear@163.com
+ * @date :  2019/7/24
+ **/
+public class ApplicationLifeCycle implements AsyncChainLifeCycle {
+    private List<AsyncChainLifeCycleListener> mLifeCycleListenerList = new ArrayList<>();
+
+    @Override
+    public void addLifeCycleListener(AsyncChainLifeCycleListener listener) {
+        mLifeCycleListenerList.remove(listener);
+        mLifeCycleListenerList.add(listener);
+    }
+
+    @Override
+    public List<AsyncChainLifeCycleListener> getLifeCycleListeners() {
+        return mLifeCycleListenerList;
+    }
+    @Override
+    public void removeLifeCycleListener(AsyncChainLifeCycleListener listener) {
+        mLifeCycleListenerList.remove(listener);
+    }
+}
